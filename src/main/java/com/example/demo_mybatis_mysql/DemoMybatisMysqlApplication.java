22da88a50a931d19f7217b7cc57e5355f218b467
keikun <>
2023-11-29 18:29:27 +0900

    参考サイトより、docker composeup 後、springbootが起動しているコンテナがRunning状態となるなるところまで実施

---
+++ src/main/java/com/example/demo_mybatis_mysql/DemoMybatisMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_mybatis_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatisMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMybatisMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_mybatis_mysql/DemoMybatisMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_mybatis_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMybatisMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
